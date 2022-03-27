14ce51fa3c52798b5409ed317f1c2af414616508
Ryotruuser <ryonantruuser@gmail.com>
2022-03-27 16:59:58 -0300

    adding profile icon n drop btn

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    var x = document.getElementById("accountMenu");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
